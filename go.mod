0dd361f853a96464db6e1c2145d5e8083ac097e9
olegz-codefresh <olegz@codefresh.io>
2020-12-16 17:57:09 +0200

    Initial commit

--- go.mod
+++ go.mod
@@ -3,5 +3,27 @@ module github.com/codefresh-io/cf-gitops-controller
 go 1.14
 
 require (
+	github.com/Masterminds/goutils v1.1.0 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/Masterminds/sprig v2.22.0+incompatible
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/gookit/color v1.2.7
+	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/mitchellh/copystructure v1.0.0 // indirect
+	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	github.com/pelletier/go-toml v1.8.0 // indirect
+	github.com/spf13/afero v1.3.2 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/cobra v1.0.0
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/viper v1.7.0
+	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b // indirect
+	golang.org/x/sys v0.0.0-20200722175500-76b94024e4b6 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	gopkg.in/ini.v1 v1.57.0 // indirect
+	k8s.io/api v0.17.0
+	k8s.io/apimachinery v0.17.0
+	k8s.io/client-go v0.17.0
 )
---
+++ pkg/kube/kube.go
@@ -0,0 +1,145 @@
+package kube
+
+import (
+	"fmt"
+	"io/ioutil"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type (
+	Kube interface {
+		buildClient() (*kubernetes.Clientset, error)
+		CreateResources(string) error
+		CreateNamespace() error
+		NamespaceExists() (bool, error)
+	}
+
+	kube struct {
+		contextName      string
+		namespace        string
+		pathToKubeConfig string
+		clientSet        *kubernetes.Clientset
+	}
+
+	Options struct {
+		ContextName      string
+		Namespace        string
+		PathToKubeConfig string
+	}
+)
+
+func New(o *Options) (Kube, error) {
+	client := &kube{
+		contextName:      o.ContextName,
+		namespace:        o.Namespace,
+		pathToKubeConfig: o.PathToKubeConfig,
+	}
+	clientSet, err := client.buildClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	client.clientSet = clientSet
+
+	return client, nil
+}
+
+func (k *kube) buildClient() (*kubernetes.Clientset, error) {
+	var config *rest.Config
+	var err error
+	config, err = clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{ExplicitPath: k.pathToKubeConfig},
+		&clientcmd.ConfigOverrides{
+			CurrentContext: k.contextName,
+			Context: clientcmdapi.Context{
+				Namespace: k.namespace,
+			},
+		}).ClientConfig()
+
+	if err != nil {
+		return nil, err
+	}
+	return kubernetes.NewForConfig(config)
+}
+
+func (k *kube) NamespaceExists() (bool, error) {
+	var exists = false
+	namespace, err := k.clientSet.CoreV1().Namespaces().Get(k.namespace, metav1.GetOptions{})
+	if err != nil {
+		return exists, err
+	}
+	if namespace != nil {
+		exists = true
+	}
+	return exists, nil
+}
+
+func (k *kube) CreateNamespace() error {
+	var namespace = v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: k.namespace,
+		},
+	}
+	_, err := k.clientSet.CoreV1().Namespaces().Create(&namespace)
+	return err
+}
+
+func (k *kube) CreateResources(manifestPath string) error {
+	var err error
+	templatesMap, err := buildTemplatesFromManifest(manifestPath)
+	if err != nil {
+		return err
+	}
+	var templatesValues map[string]interface{}
+
+	kubeObjects, parsedTemplates, err := BuildObjectsFromTemplates(templatesMap, templatesValues)
+	if kubeObjects != nil {
+		fmt.Print(parsedTemplates)
+	}
+	if err != nil {
+		// @todo
+		fmt.Print(parsedTemplates)
+	}
+
+	var resources = v1.ResourceQuota{}
+	_, err = k.clientSet.CoreV1().ResourceQuotas(k.namespace).Create(&resources)
+	return err
+}
+
+func buildTemplatesFromManifest(manifestPath string) (map[string]string, error) {
+	var templatesMap = map[string]string{}
+	var manifestByte []byte
+	var err error
+	if strings.HasPrefix(manifestPath, "http://") || strings.HasPrefix(manifestPath, "https://") {
+		manifestByte, err = downloadManifest(manifestPath)
+	} else {
+		manifestByte, err = ioutil.ReadFile(manifestPath)
+	}
+	if err != nil {
+		return templatesMap, err
+	}
+	templates := strings.Split(string(manifestByte), "\n---\n")
+	for n, tpl := range templates {
+		templatesMap["template_"+strconv.Itoa(n)+".yaml"] = tpl
+	}
+	return templatesMap, err
+}
+
+func downloadManifest(url string) ([]byte, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return []byte{}, err
+	}
+	defer response.Body.Close()
+	return ioutil.ReadAll(response.Body)
+}
