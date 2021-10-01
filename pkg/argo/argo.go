c5f2e885972beb7eacfd76a8b93351d44d1e022c
olegz <olegz@codefresh.io>
2021-10-01 17:47:38 +0300

    Default app

--- pkg/argo/argo.go
+++ pkg/argo/argo.go
@@ -9,7 +9,7 @@ func CreateDefaultApp(argoApi *argoSdk.Argo) error {
 	requestOptions.Metadata.Name = "default"
 	requestOptions.Spec.Project = "default"
 	requestOptions.Spec.Destination.Name = ""
-	requestOptions.Spec.Destination.Namespace = ""
+	requestOptions.Spec.Destination.Namespace = "default"
 	requestOptions.Spec.Destination.Server = "https://kubernetes.default.svc"
 	requestOptions.Spec.Source.RepoURL = "https://github.com/argoproj/argocd-example-apps.git"
 	requestOptions.Spec.Source.Path = "guestbook"
